25ecb609014a503b75e2eeb0eb791e7cefd52269
aseri-yuvraj <yuvrajaaseri121@gmail.com>
2023-11-09 17:07:00 +0530

    updates

--- appBackend/src/Lib/mongo.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export const dbConnect = ()=>{ 
-    mongoose.connect("mongodb://localhost:27017/",{dbName:"DCut"})
-    .then(() => console.log('Mongo DB connected!'))
-    .catch((e)=> console.log('Mongo DB connection failed!', e))
-};
--- appBackend/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./auth/auth.router";
-export * from "./global/data.router";
-export * from "./broker/broker.router";
-export * from "./broker/broker.productConfigrator.router";
-export * from "./employer/employer.routes";
-export * from "./employee/employee.routes";
